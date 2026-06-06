9e16b269fbeb2ca8582f8a811680e0e7755248fa
solaluset <60041069+solaluset@users.noreply.github.com>
2026-06-06 20:02:16 +0300

    Implement etime command

--- commands/etime.py
+++ commands/etime.py
@@ -5,11 +5,21 @@ from modules.i18n import t
 
 
 @context_menu(
-    name=locale_str("..."),
+    name=locale_str("etime.title"),
 )
-async def get_edit_time(ctx, message: discord.Message):
-    language = await ctx.client.get_language(ctx.guild)
-    await ctx.response.send_message(t("#", language), ephemeral=True)
+async def get_edit_time(inter, message: discord.Message):
+    language = await inter.client.get_language(inter.guild)
+
+    time = message.edited_at
+    if time is None:
+        await inter.response.send_message(
+            t("etime.not-edited", language), ephemeral=True
+        )
+    else:
+        await inter.response.send_message(
+            t("etime.edited", language, time=f"<t:{int(time.timestamp())}:F>"),
+            ephemeral=True,
+        )
 
 
 async def setup(bot):
